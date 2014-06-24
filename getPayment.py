6e2873a8a9fa131ee4d5b99c87d9bec352091ce9
visitor <visitor@tid.es>
2014-06-24 09:43:06 +0200

    Added getPayment script to check existing payment information

---
+++ getPayment.py
@@ -0,0 +1,40 @@
+import sys
+import operator
+import requests
+import json
+import ConfigParser
+
+
+# merchant and environment configuration
+config = ConfigParser.ConfigParser()
+config.read('merchant.cfg')
+apikey = config.get('merchant','apikey')
+base_endpoint =  config.get('environment','base_endpoint') 
+
+
+# generate GET request to get transaction info
+def generate_request(id):
+    endpoint = base_endpoint + "/payments/" + id
+    headers = {'Content-type': 'application/json', 'apikey': apikey}
+    params = {'apikey' : apikey}
+
+    r = requests.get(endpoint, headers=headers, params=params, verify=False)
+    
+    print "REQUEST : GET " + endpoint
+    print "RESPONSE : "+ str(r.status_code)
+    print r.text
+
+
+def main():   
+
+    if len(sys.argv) != 2:
+         print 'usage: ./getPayments.py id'
+         sys.exit(1)
+
+    id = sys.argv[1] 
+     
+    generate_request(id) 
+
+
+if __name__ == '__main__':
+    main()
